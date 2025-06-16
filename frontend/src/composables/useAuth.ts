3676665a2b8ae410f053f47f58eea2d998ed79d4
Gerardo Gael Rojas Ramos <gerardo.rojasramos@ucr.ac.cr>
2025-06-15 22:36:00 -0400

    Eliminar el composable useAuth y su lógica de autenticación

--- frontend/src/composables/useAuth.ts
+++
@@ -1,6 +0,0 @@
-import { loginToTVDB } from '@/services/authServices';
-
-export async function useAuth(apiKey: string, pin: string) {
-  const token = await loginToTVDB(apiKey, pin);
-  localStorage.setItem('tvdbToken', token);
-}
